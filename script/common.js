6244f96df85200f3a217b7ecd05658cae8617a45
bymelod <bymelod@naver.com>
2023-07-25 17:29:00 +0900

    0725

---
+++ script/common.js
@@ -0,0 +1,24 @@
+$(function () {
+
+ 
+    
+    // $('.global-menu').click(function(){
+    //     $('.global-menu ul').addClass('active')
+    // })
+    // $('.close').click(function(){
+    //     $('.global-menu ul').removeClass('active')
+    // })
+
+    // $('.slider').slick();
+
+
+//       $('.slider').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//   });
+
+
+
+})
